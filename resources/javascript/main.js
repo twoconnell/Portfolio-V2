83e1c1b6f4b034f9be031e589889552860b148e9
twoconnell <109411673+twoconnell@users.noreply.github.com>
2022-11-13 17:12:27 +0000

    cookies, about-me section update, updated icons

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,4 +1,82 @@
 // ------------------------------------------------------------------------
+//  Prevent scrolling
+// ------------------------------------------------------------------------
+
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+
+var supportsPassive = false;
+try {
+  window.addEventListener(
+    "test",
+    null,
+    Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      },
+    })
+  );
+} catch (e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+disableScroll();
+
+
+// ------------------------------------------------------------------------
+//  Cookie popup
+// ------------------------------------------------------------------------
+
+const cookieBox = document.querySelector(".cookie-popup");
+const cookieYes = document.querySelector(".cookie-button-yes");
+const cookieNo = document.querySelector(".cookie-button-no");
+
+cookieYes.addEventListener("click", () => {
+  document.cookie = "CookieBy=twoconnell.com; max-age=" + 60 * 60 * 24 * 30; //Cookie set for 1 month
+  cookieBox.classList.add("hide");
+  enableScroll();
+});
+
+cookieNo.addEventListener("click", () => {
+  cookieBox.classList.add("hide");
+  enableScroll();
+});
+
+if (document.cookie) {
+  cookieBox.classList.add("hide");
+  enableScroll();
+} else {
+  cookieBox.classList.remove("hide");
+}
+
+// ------------------------------------------------------------------------
 //  Locally store theme choice
 // ------------------------------------------------------------------------
 
@@ -7,7 +85,9 @@ const colorThemes = document.querySelectorAll('[name="theme"]');
 //store theme
 
 const storeTheme = function (theme) {
-  localStorage.setItem("theme", theme);
+  if (document.cookie) { //Checks for cookie before setting localStorage
+    localStorage.setItem("theme", theme);
+  }
 };
 
 const getTheme = function () {
@@ -17,8 +97,6 @@ const getTheme = function () {
       themeOption.checked = true;
     }
   });
-  // for no :has support
-  document.documentElement.className = theme;
 };
 
 colorThemes.forEach((themeOption) => {
